4ebd5e38ea066ada93aaf0f7f7437006040739bf
Yogendra Singh <yog27ray@gmail.com>
2020-03-11 11:13:08 +0530

    commit

--- release.sh
+++ release.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
-cd dist
-rm *
+cd distcd
+git reset --hard
+git checkout dist
+rm -r *
 cd ..
 VERSION=$(npm version patch)
 npm i
 tsc -p ./
+cp package.json ./dist/package.json
+cp package-lock.json ./dist/package-lock.json
 cd dist
+git add *
 git commit -m "$VERSION"
 git push origin dist
