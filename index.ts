34354b0b2af55874a7626ae9ac8017d824312fb7
Yogendra Singh <yog27ray@gmail.com>
2020-03-11 13:20:28 +0530

    commit

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import { EventItem } from 'queue-manager/event-manager';
-import { SlaveEventScheduler } from 'queue-manager/scheduler-slave/slave-event-scheduler';
-import { MasterEventScheduler } from 'queue-manager/scheduler-master/master-event-scheduler';
-import { QueueManager } from 'queue-manager/queue-manager';
+import { EventItem } from './queue-manager/event-manager';
+import { SlaveEventScheduler } from './queue-manager/scheduler-slave/slave-event-scheduler';
+import { MasterEventScheduler } from './queue-manager/scheduler-master/master-event-scheduler';
+import { QueueManager } from './queue-manager/queue-manager';
 
 export { QueueManager, MasterEventScheduler, SlaveEventScheduler, EventItem };
