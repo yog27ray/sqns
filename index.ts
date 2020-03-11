506ea5b4d21f43a776d18ad4ef7c9d58ad66d706
yog27ray <yog27ray@gmail.com>
2020-03-11 13:17:25 +0530

    Delete index.ts

--- index.ts
+++
@@ -1,5 +0,0 @@
-import { QueueManager } from './queue-manager/queue-manager';
-import { SlaveEventScheduler } from './queue-manager/scheduler-slave/slave-event-scheduler';
-import { MasterEventScheduler } from './queue-manager/scheduler-master/master-event-scheduler';
-
-export { QueueManager, MasterEventScheduler, SlaveEventScheduler };
