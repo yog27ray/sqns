ed1a96a2d558ad58d093f2861918736d6ead4cc5
Yogendra Singh <yog27ray@gmail.com>
2020-03-11 13:19:39 +0530

    0.0.11

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const event_manager_1 = require("queue-manager/event-manager");
+const event_manager_1 = require("./queue-manager/event-manager");
 exports.EventItem = event_manager_1.EventItem;
-const slave_event_scheduler_1 = require("queue-manager/scheduler-slave/slave-event-scheduler");
+const slave_event_scheduler_1 = require("./queue-manager/scheduler-slave/slave-event-scheduler");
 exports.SlaveEventScheduler = slave_event_scheduler_1.SlaveEventScheduler;
-const master_event_scheduler_1 = require("queue-manager/scheduler-master/master-event-scheduler");
+const master_event_scheduler_1 = require("./queue-manager/scheduler-master/master-event-scheduler");
 exports.MasterEventScheduler = master_event_scheduler_1.MasterEventScheduler;
-const queue_manager_1 = require("queue-manager/queue-manager");
+const queue_manager_1 = require("./queue-manager/queue-manager");
 exports.QueueManager = queue_manager_1.QueueManager;
 //# sourceMappingURL=index.js.map
