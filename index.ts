692df21c88eb6e5c0ae6b5e0a105b2f108f1156e
Yogendra Singh <yog27ray@gmail.com>
2021-04-15 19:49:41 +0530

    fix: build fix

--- index.ts
+++ index.ts
@@ -2,5 +2,6 @@ import { SQNS } from './src/sqns/s-q-n-s';
 import { SQNSClient } from './src/sqns/s-q-n-s-client';
 import { ManagerEventScheduler } from './src/sqns/scheduler/scheduler-manager/manager-event-scheduler';
 import { WorkerEventScheduler } from './src/sqns/scheduler/scheduler-worker/worker-event-scheduler';
+import { Database } from './src/sqns/common/database';
 
-export { ManagerEventScheduler, WorkerEventScheduler, SQNSClient, SQNS };
+export { ManagerEventScheduler, WorkerEventScheduler, SQNSClient, SQNS, Database };
