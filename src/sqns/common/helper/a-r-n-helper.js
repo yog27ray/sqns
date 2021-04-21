c99e911508e0bce7944fe12436606d472a009af2
Yogendra Singh <yog27ray@gmail.com>
2021-04-21 23:36:42 +0530

    v2.0.14

--- src/sqns/common/helper/a-r-n-helper.js
+++ src/sqns/common/helper/a-r-n-helper.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ARNHelper = void 0;
 class ARNHelper {
     static findResourceClassOfARN(resourceARN) {
-        if (new RegExp('^arn:sqns:sns:[a-zA-Z0-9]*:[a-zA-Z0-9]*:[a-zA-Z0-9]*$').exec(resourceARN)) {
+        if (new RegExp('^arn:sqns:sns:[a-zA-Z0-9]*:[a-zA-Z0-9]*:[a-zA-Z0-9_]*$').exec(resourceARN)) {
             return 'Topic';
         }
         return undefined;
