9cb88593b86ccc935339774a5930bc1b7346289d
Yogendra Singh <yog27ray@gmail.com>
2021-08-20 12:48:32 +0530

    v2.1.17

--- src/sqns/common/database/mongodb/mongo-d-b-adapter.js
+++ src/sqns/common/database/mongodb/mongo-d-b-adapter.js
@@ -63,6 +63,7 @@ class MongoDBAdapter {
         };
         log.info('DB Fetch ', query);
         const mongoDocuments = await this.connection.find(MongoDBAdapter.Table.Event, query, { eventTime: -1 }, { limit });
+        log.info('DB Fetch Result length: ', mongoDocuments.length);
         return mongoDocuments.map((mongoDocument) => new event_item_1.EventItem(MongoDBAdapter.dbToSystemItem(mongoDocument)));
     }
     async getQueues(queueARNPrefix) {
