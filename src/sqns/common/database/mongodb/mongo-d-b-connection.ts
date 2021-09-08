4c1b8fdbad0fb9f03d3619097e3ed1fccdce5fa3
Yogendra Singh <yog27ray@gmail.com>
2021-09-08 09:47:27 +0530

    fix: Skip, limit and sort in find query.

--- src/sqns/common/database/mongodb/mongo-d-b-connection.ts
+++ src/sqns/common/database/mongodb/mongo-d-b-connection.ts
@@ -48,7 +48,11 @@ class MongoDBConnection {
       query._id = query.id;
       delete query.id;
     }
-    return this.getDB().collection(tableName).find(query).sort(sort).skip(skip || 0).limit(limit || 100).toArray();
+    return this.getDB().collection(tableName).find(query)
+      .sort(sort)
+      .skip(skip || 0)
+      .limit(limit || 100)
+      .toArray();
   }
 
   async findOne(tableName: string, filter_: KeyValue = {}): Promise<KeyValue> {
