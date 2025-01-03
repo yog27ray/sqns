e061a00ceeaccbb48d5229b6bb50d39e77499c40
yog27ray <yog27ray@gmail.com>
2025-01-03 13:59:24 +0530

    node-fetch and logger4node

--- src/client/request-client/request-client.ts
+++ src/client/request-client/request-client.ts
@@ -3,7 +3,7 @@ import { SQNSError } from '../auth/s-q-n-s-error';
 
 type RequestInit = Parameters<typeof fetch>[1];
 type BodyInit = RequestInit['body'];
-type HeaderInit = RequestInit['headers'];
+type HeadersInit = RequestInit['headers'];
 
 class RequestClient {
   private static MAX_RE_ATTEMPT = 3;
